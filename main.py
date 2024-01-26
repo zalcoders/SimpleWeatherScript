b670c100340eb13ed172011a65e3b03d84d9692b
Bahman Shadmehr <info@bshadmehr.me>
2024-01-26 17:06:48 +0300

    Added an option for the user to get our recomendation and added a print so that user can see our recomendation

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from data import WEATHER_DATA
 
-def get_weather(city):
+def get_weather(city, show_recomendations):
     """Retrieve weather data for a given city."""
-    return WEATHER_DATA.get(city.title(), "No data available for this city.")
+    data = WEATHER_DATA.get(city.title(), "No data available for this city.")
+    if show_recomendations:
+        recomendation = get_recommendation(data["condition"])
+    else:
+        recomendation = None
+    return data, recomendation
 
 def get_recommendation(condition):
     if condition == "Rainy":
@@ -14,8 +19,14 @@ def get_recommendation(condition):
 
 def main():
     city = input("Enter the name of the city to get its current weather: ")
-    weather = get_weather(city)
+    wants_recomendations = input("Do you want to see our recomendation for today?(y/n): ")
+    if wants_recomendations == "y":
+        show_recomendations = True
+    else:
+        show_recomendations = False
+    weather, final_recomendation = get_weather(city, show_recomendations)
     print(f"Weather in {city.title()}: {weather}")
+    print(f"And our recomendation is: {final_recomendation}")
 
 if __name__ == "__main__":
     main()
